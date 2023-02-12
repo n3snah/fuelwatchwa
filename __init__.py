9db5cea58a050286bbba774f72fc5a9d5be381da
Ross Hansen <hansen.ross@live.com.au>
2023-02-12 21:28:08 +0800

    feat: Adding a en.json translations file copied from strings.json feat: adding a setup function to __init__.py feat: adding a semi-working config_flow.py feat: Adding a strings.json as the base template for translations.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
 """The fuelwatchwa sensor integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Something"""
+
+    return True
--- config_flow.py
+++ config_flow.py
@@ -1,9 +1,33 @@
 from homeassistant import config_entries
 from .const import DOMAIN
+import voluptuous as vol
+
+STEP_USER_DATA_SCHEMA= {
+            vol.Required("suburb"): str
+        }
 
 
-class FuelwatchwaConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """FuelWatch WA config flow."""
     # The schema version of the entries that it creates
     # Home Assistant will call your migrate method if the version changes
     VERSION = 1
+
+    def __init__(self):
+        """Initialize the Config flow for FuelWatch WA"""
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+
+        if user_input is not None:
+            # Validate User input
+            valid = True
+            if valid:
+                return self.async_create_entry(
+                    title="FuelWatchWA",
+                    data=user_input
+                )
+
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema(STEP_USER_DATA_SCHEMA), errors=errors
+        )
