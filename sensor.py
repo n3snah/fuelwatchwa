dfd85b6f41efd0ce7d265503b7e8174e60d03c0d
Ross Hansen <hansen.ross@live.com.au>
2023-02-12 14:03:47 +0800

    feat: initial setup of a basic sensor

---
+++ sensor.py
@@ -0,0 +1,37 @@
+"""Platform for sensor integration."""
+from __future__ import annotations
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
+from homeassistant.const import CURRENCY_CENT
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None
+) -> None:
+    """Set up the sensor platform."""
+    add_entities([FuelPriceToday()])
+
+
+class FuelPriceToday(SensorEntity):
+    """Representation of a Sensor."""
+
+    _attr_name = "Fuel Price today"
+    _attr_native_unit_of_measurement = CURRENCY_CENT
+    _attr_device_class = SensorDeviceClass.MONETARY
+    _attr_state_class = SensorStateClass.MEASUREMENT
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._attr_native_value = 161.9
