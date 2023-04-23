efe643ca4f0f7826214e7ed3c5ac1687956bb4cb
Ross Hansen <hansen.ross@live.com.au>
2023-04-23 18:14:14 +0800

    feat: Adding more tests for sensor.py broken: Tests not complete and causing pytest issues

--- tests/unit/test_sensor.py
+++ tests/unit/test_sensor.py
@@ -4,4 +4,86 @@ import pytest
 
 # Append parent directory to Python path
 sys.path.append("..")
-from fuelwatchwa.sensor import async_setup_entry
+from fuelwatchwa.sensor import async_setup_entry, FuelWatchSensor, FuelPrice, FuelStationName, FuelStationLocation, FuelStationAddress
+
+def test_async_setup_entry():
+    assert True
+
+class MockConfigEntry:
+    def __init__(self, data):
+        self.data = data
+
+class TestFuelWatchSensor:
+    def test_fuel_type():
+        fuel_type = 1
+        suburb = "Test Suburb"
+        day = "Test Day"
+        xml_key = "Test Key"
+        hass = None
+        config_entries = MockConfigEntry({'product': fuel_type, 'suburb': suburb, 'day': day})
+
+        fuel_watch_sensor = FuelWatchSensor(hass, config_entries, "attr_name", xml_key)
+
+        # Test fuel_type property
+        assert fuel_watch_sensor.fuel_type == fuel_type
+
+    def test_suburb(self):
+        fuel_type = 1
+        suburb = "Test Suburb"
+        day = "Test Day"
+        xml_key = "Test Key"
+        hass = None
+        config_entries = MockConfigEntry({'product': fuel_type, 'suburb': suburb, 'day': day})
+
+        fuel_watch_sensor = FuelWatchSensor(hass, config_entries, "attr_name", xml_key)
+
+        # Test suburb property
+        assert fuel_watch_sensor.suburb == suburb
+
+    def test_day(self):
+        fuel_type = 1
+        suburb = "Test Suburb"
+        day = "Test Day"
+        xml_key = "Test Key"
+        hass = None
+        config_entries = MockConfigEntry({'product': fuel_type, 'suburb': suburb, 'day': day})
+
+        fuel_watch_sensor = FuelWatchSensor(hass, config_entries, "attr_name", xml_key)
+
+        # Test day property
+        assert fuel_watch_sensor.day == day
+
+    def test_update(self):
+        fuel_type = 1
+        suburb = "Test Suburb"
+        day = "Test Day"
+        xml_key = "Test Key"
+        hass = None
+        config_entries = MockConfigEntry({'product': fuel_type, 'suburb': suburb, 'day': day})
+
+        fuel_watch_sensor = FuelWatchSensor(hass, config_entries, "attr_name", xml_key)
+
+        # Mock the FuelWatch API query and get_xml methods
+        fuel_watch_sensor.api.query = lambda **kwargs: None
+        fuel_watch_sensor.api.get_xml = [{"Test Key": "Test Value"}]
+
+        # Test update method
+        fuel_watch_sensor.update()
+        assert fuel_watch_sensor.xml_query == fuel_watch_sensor.api.get_xml
+        assert fuel_watch_sensor._attr_native_value == "Test Value"
+
+def test_FuelPrice():
+    assert isinstance(FuelPrice, type)
+    assert hasattr(FuelPrice, '__init__')
+
+def test_FuelStationName():
+    assert isinstance(FuelStationName, type)
+    assert hasattr(FuelStationName, '__init__')
+
+def test_FuelStationLocation():
+    assert isinstance(FuelStationLocation, type)
+    assert hasattr(FuelStationLocation, '__init__')
+
+def test_FuelStationAddress():
+    assert isinstance(FuelStationAddress, type)
+    assert hasattr(FuelStationAddress, '__init__')
