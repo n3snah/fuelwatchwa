c32ce0b3287f6868668257d7facb85429b8252ad
Ross Hansen <hansen.ross@live.com.au>
2023-04-25 05:46:36 +0800

    fix: corrected numerous pylint warnings

--- tests/unit/test_config_flow.py
+++ tests/unit/test_config_flow.py
@@ -1,17 +1,16 @@
 '''Unit tests for the config_flow file'''
 import sys
-import pytest
 import inspect
 from homeassistant import config_entries
 
 # Append parent directory to Python path
 sys.path.append("..")
-from fuelwatchwa.const import DOMAIN
 from fuelwatchwa.config_flow import ConfigFlow
 
 def test_ConfigFlow():
-  assert issubclass(config_entries.ConfigFlow, config_entries.ConfigFlow)
+    """Unit tests for ConfigFlow class"""
+    assert issubclass(config_entries.ConfigFlow, config_entries.ConfigFlow)
 
-  assert hasattr(config_entries.ConfigFlow, 'async_step_user')
-  assert inspect.iscoroutinefunction(config_entries.ConfigFlow.async_step_user)
-  assert ConfigFlow.VERSION == 1
+    assert hasattr(config_entries.ConfigFlow, 'async_step_user')
+    assert inspect.iscoroutinefunction(config_entries.ConfigFlow.async_step_user)
+    assert ConfigFlow.VERSION == 1
