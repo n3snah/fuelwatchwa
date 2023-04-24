7fa55382633a1fbf54be1dcf968772e5ccc86f04
Ross Hansen <hansen.ross@live.com.au>
2023-04-25 05:51:17 +0800

    fix: Resolved a number of Pylint warnings

--- tests/unit/test_init.py
+++ tests/unit/test_init.py
@@ -1,6 +1,5 @@
 '''Pytests for the package / init'''
 import sys
-import pytest
 import inspect
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
@@ -13,17 +12,27 @@ from fuelwatchwa.const import DOMAIN
 # tests/unit/test_init.py
 
 def test_init():
+    """Unit tests for package init method"""
     assert DOMAIN == 'fuelwatchwa'
 
 def test_async_setup_entry():
+    """Unit tests for package async_setup_entry method"""
     import fuelwatchwa.__init__
     assert hasattr(fuelwatchwa.__init__, 'async_setup_entry')
     assert inspect.iscoroutinefunction(async_setup_entry)
 
     assert inspect.signature(async_setup_entry) == inspect.Signature(
         parameters=[
-            inspect.Parameter("hass", inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation=HomeAssistant),
-            inspect.Parameter("entry", inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation=ConfigEntry),
+            inspect.Parameter(
+                "hass",
+                inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                annotation=HomeAssistant
+            ),
+            inspect.Parameter(
+                "entry",
+                inspect.Parameter.POSITIONAL_OR_KEYWORD,
+                annotation=ConfigEntry
+            ),
         ],
         return_annotation=bool
     )
